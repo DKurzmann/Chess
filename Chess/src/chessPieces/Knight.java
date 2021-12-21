81ac77bd50661990e61bbf024b12ea10ef9d424c
Kurzmann <david.kurzmann@sap.com>
2021-12-21 19:27:23 +0100

    cleaned up Knight

--- Chess/src/chessPieces/Knight.java
+++ Chess/src/chessPieces/Knight.java
@@ -37,27 +37,48 @@ public class Knight extends Piece implements IPiece {
 
 	@Override
 	public List<Move> calculateMoves(Board board) {
+		List<Integer>positions = getPositions();
+		List<Square> squares = getSquares(positions, board);
+		List<Move> moves = getMoves(squares); 
+		return moves;
+	}
+	
+	private List<Move> getMoves(List<Square> squares) {
 		List<Move> moves = new ArrayList<Move>();
-		for(final int candidate_move : CANDIDATE_MOVES) {
-			int candidate_position = current_position + candidate_move;
-			//is in board range?
-			if(candidate_position >= 0 && candidate_position <= 64) {
-				final Square candidateSquare = board.getSquare(candidate_position);
-				//isOccupied?
-				if(candidateSquare.isOccupied()) {
-					//occupied with other players piece?
-					if(!candidateSquare.getPiece().pieceColor.equals(this.pieceColor)){
-						//This is a MOVE!
-						moves.add(new Move());
-					}
-				} else {
-				//not occupied?
-					//this is a MOVE!
-						moves.add(new Move());
-				}
+		for(Square square : squares) {
+			if( square.isOccupied() == false) {
+				moves.add(new Move());
+			} else if(isOccupiedWithOtherPlayersPiece(square)){
+				moves.add(new Move());
 			}
 		}
 		return moves;
+	}
+
+	private List<Square> getSquares(List<Integer> positions, Board board) {
+		List<Square> squares = new ArrayList<Square>();
+		for(final int position : positions) {
+			if(isValidPosition(position)) {
+				squares.add(board.getSquare(position));
+			}
+		}
+		return squares;
+	}
+
+	private List<Integer> getPositions() {
+		List<Integer> positions = new ArrayList<Integer>();
+		for(final int candidate_move : CANDIDATE_MOVES) {
+			positions.add(current_position + candidate_move);
+		}
+		return positions;
+	}
+
+	private boolean isValidPosition(int position) {
+		return position >= 0 && position <= 64;
+	}
+	
+	private boolean isOccupiedWithOtherPlayersPiece(Square square) {
+		return square.isOccupied() && !square.getPiece().pieceColor.equals(this.pieceColor);
 	}
 
 }
